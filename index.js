fa4a7d72f002fa40ad860b1e6aeb14717674c766
qovciu <372903575@qq.com>
2015-06-17 07:41:59 +0800

    eX

---
+++ index.js
@@ -0,0 +1,24 @@
+﻿var app = require('./lib/main.js');
+var mysql = require('./mysql.js');
+
+app.baseUrl(__dirname + '/public');
+app.use('/', function (req, res) {
+    var params;
+    params = req.query.mode || req.body.mode;
+    if (!params) {
+        return (console.log('参数为空!')) || (res.end('what the fuck!?'));
+    }
+    params = params.split(',');
+    if (params[0] === 'mysql') {
+        params.shift();
+        return mysql.usselect(params, function (results) {
+            return res.end(JSON.stringify(results));
+        });
+    } else {
+        return res.end('what the fuck!?');
+    }
+})
+app.use('/bbb', function (req, res) {
+    res.end('bbb');
+})
+app.start(8080);
---
+++ test/setting.js
@@ -0,0 +1,23 @@
+﻿var app = require('../lib/main.js');
+var assert = require("assert");
+describe('app setting', function () {
+    describe('app.baseUrl', function () {
+        it('when set baseUrl should be baseUrl', function () {
+            var baseUrl = '/';
+            app.baseUrl(baseUrl);
+            assert.equal(baseUrl, app.settings['basename']);
+        })
+        it('when not set baseUrl should be /', function () {
+            assert.equal('/', app.settings['basename']);
+        })
+    })
+    describe('app.route', function () {
+        it('when set url should have url', function () {
+            var baseUrl = '/aa';
+            app.use(baseUrl, function start() { 
+                return 'start';
+            });
+            assert.equal('start', app.settings['router'][baseUrl]());
+        })
+    })
+})
