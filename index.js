a8922b6c3bb4ff70acabf51b1159d6d6de210e60
qv <foru.fy@gmail.com>
2015-06-19 08:44:27 +0800

    example

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 ﻿var app = require('./lib/main.js');
 
 app.baseUrl('/');
-app.use('/', function (req, res) {
-    var params;
-    params = req.query.mode || req.body.mode;
-    if (!params) {
-        return (console.log('参数为空!')) || (res.end('what the fuck!?'));
-    }
-    params = params.split(',');
-    if (params[0] === 'mysql') {
-        params.shift();
-        return mysql.usselect(params, function (results) {
-            return res.end(JSON.stringify(results));
-        });
-    } else {
-        return res.end('what the fuck!?');
-    }
-})
 app.use('/bbb', function (req, res) {
     res.end('bbb');
 })
